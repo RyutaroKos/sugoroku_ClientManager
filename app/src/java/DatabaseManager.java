364017732d380d47c1352da65e0410c3bf394f6b
fumofumo3 <bp19027@shibaura-it.ac.jp>
2021-12-22 14:45:33 +0900

    DatabaseManagerにJavadocを追加

--- app/src/java/DatabaseManager.java
+++ app/src/java/DatabaseManager.java
@@ -1,18 +1,30 @@
 class DatabaseManager
 {
-    String url;
-    String port;
+	private static final String url;
+	private static final String port;
+
 
     String getPassword(String userID)
     {
 
     }
 
+    /**
+     * ユーザ登録されているかの判定をするメソッド
+     * @param id ユーザID
+     * @param pwd パスワード
+     * @return boolean
+     */
     boolean searchUser(String id,String pwd)
     {
 
     }
 
+    /**
+     * ユーザを登録するメソッド
+     * @param id ユーザID
+     * @param pwd パスワード
+     * @return boolean*/
     boolean signUp(String id,String pwd)
     {
 
