fcb15f5c0f944549f90ee76e872149135b974e60
fumofumo3 <bp19027@shibaura-it.ac.jp>
2022-01-03 20:33:23 +0900

    Userクラスをsessionへ対応

--- app/src/java/User.java
+++ app/src/java/User.java
@@ -10,27 +10,29 @@
  #define inGame 3
  //
 
+ import javax.websocket.Session;
+
 class User
 {
     private String userName;
-    private String webSocketID;
-    private int status; //　0=ロビー外　１=ランダムロビー内　2=プライベートロビー内　3=ゲーム中
+    private Session session;
+    private int status; //0=ロビー外,１=ランダムロビー内,2=プライベートロビー内,3=ゲーム中
     private String lobbyID;
 
-    User(String userName,String webSocketID)
+    User(String userName, Session session)
     {
-        this.userName=userName;
-        this.webSocketID=webSocketID;
-        this.status=outLobby;
-        this.lobbyID=null;
+        this.userName = userName;
+        this.session = session;
+        this.status = outLobby;
+        this.lobbyID = null;
     }
 
-    User(String userName,String webSocketID,int status,String lobbyID)
+    User(String userName, Session session, int status, String lobbyID)
     {
-        this.userName=userName;
-        this.webSocketID=webSocketID;
-        this.status=status;
-        this.lobbyID=lobbyID;
+        this.userName = userName;
+        this.session = session;
+        this.status = status;
+        this.lobbyID = lobbyID;
     }
 
     public String getName()
@@ -38,19 +40,19 @@ class User
         return this.userName;
     }
 
-    public String getWebSocketID()
+    public Session getSession()
     {
-        return this.webSocketID;
+        return this.session;
     }
 
-    public void setWebSocketID(String SocketID)
+    public void setSession(Session session)
     {
-        this.webSocketID=SocketID;
+        this.session = session;
     }
 
     public void setStatus(int statusID)
     {
-        this.status=statusID;
+        this.status = statusID;
     }
 
     public int getStatus()
@@ -60,7 +62,7 @@ class User
 
     public void setLobbyID(String LobbyID)
     {
-        this.lobbyID=LobbyID;
+        this.lobbyID = LobbyID;
     }
 
     public String getLobbyID()
