4f4f48e24d72666826b0f0ba50c276d6c76911e3
fumofumo3 <bp19027@shibaura-it.ac.jp>
2022-01-09 16:13:05 +0900

    ComManagerの更新

--- app/src/java/ComManager.java
+++ app/src/java/ComManager.java
@@ -1,32 +1,76 @@
-import java.util.LinkedList;
+/*
+EndPointSample.java
+サーバー起動後の動作を記述する
+ */
+//package server;
+import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Queue;
-class ComManager
-{
-    Queue<String> sendQueue = new LinkedList<String>();
-    Queue<String> receiveQueue = new LinkedList<String>();
 
-    boolean connectServer(int seedServer)
-    {
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
 
+// エンドポイントは適宜変更する
+@ServerEndpoint("/test")
+public class ComManager {
+	private static ArrayList<Session> Sessions = new ArrayList<>();
+	static Queue<Message> queue = new ArrayDeque<>();
+
+
+    @OnOpen
+    public void onOpen(Session session, EndpointConfig ec) {
+    	Sessions.add(session);
+        System.out.println("[WebSocketServerSample] onOpen:" + session.getId());
     }
 
-    boolean disconnect(int seedServer)
-    {
 
+    @OnMessage
+    public void onMessage(final String message, final Session session) throws IOException {
+        System.out.println("[WebSocketServerSample] onMessage from (session: " + session.getId() + ") msg: " + message);
+
+        //Messageインスタンスを作成
+        Message receivedMessage = new Message(message,session);
+	    //Messageインスタンスをキューに保存
+        queue.add(receivedMessage);
     }
 
-    void sendMessage(String str)
-    {
 
+    @OnClose
+    public void onClose(Session session) {
+        System.out.println("[WebSocketServerSample] onClose:" + session.getId());
+    	Sessions.remove(session);
     }
 
-    void receive()
-    {
-
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("[WebSocketServerSample] onError:" + session.getId());
     }
 
-    String deq()
-    {
+	public void sendMessage(Session session, String message) {
+		System.out.println("[WebSocketServerSample] sendMessage(): " + message);
+		try {
+			// 同期送信（sync）
+			session.getBasicRemote().sendText(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    }
+	public void sendBroadcastMessage(String message) {
+		System.out.println("[WebSocketServerSample] sendBroadcastMessage(): " + message);
+		Sessions.forEach(session -> {
+			// 非同期送信（async）
+			session.getAsyncRemote().sendText(message);
+		});
+	}
+
+	public static Message deq() {
+		return queue.poll();
+	}
 }
