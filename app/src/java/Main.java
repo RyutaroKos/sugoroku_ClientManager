6eaff7292afd3429ef97d242b8ce3a9eaa43518e
fumofumo3 <bp19027@shibaura-it.ac.jp>
2022-01-12 11:42:42 +0900

    Mainクラスを追加

---
+++ app/src/java/Main.java
@@ -0,0 +1,32 @@
+//package cms;
+import org.glassfish.tyrus.server.Server;
+
+public class Main {
+	static String contextRoot = "/app";
+	static String protocol = "ws";
+	static int port = 8080;
+
+	public static void main(String[] args) {
+        Server server = new Server(protocol, port, contextRoot, null, ComManager.class);
+
+		ClientManageServer cms = new ClientManageServer();
+		ComManager.setCMS(cms);
+		Thread trd = new Thread(cms);
+
+		//System.out.println("test");
+		try
+		{
+			trd.start();
+			server.start();
+		}
+		finally
+		{
+            server.stop();
+			trd.interrupt();
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
